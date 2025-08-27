b2fa4852c5d62bfb99ad532ac63abdc648497a1e
SaaraRi <saara.rikama@gmail.com>
2025-08-27 13:22:58 +0300

    fix css import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-// import "./App.css";
+import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import Layout from "./Layout";
 import Login from "./Pages/Login";
